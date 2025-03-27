8bdbaddd2ef7456eed651e69206d78595e501c95
tasneem qanoa <simora2017@gmail.com>
2025-03-27 13:54:20 +0200

    new

--- RotateArray.cs
+++ RotateArray.cs
@@ -2,15 +2,23 @@ public class Solution
 {
    public void Rotate(int[] nums, int k)
    {
-      int count = 0;
-      while (count <= k)
+      int n = nums.Length;
+      k = k % n;
+
+      Reverse(nums, 0, n - 1);   
+      Reverse(nums, 0, k - 1);   
+      Reverse(nums, k, n - 1);   
+   }
+
+   private void Reverse(int[] nums, int start, int end)
+   {
+      while (start < end)
       {
-         int temp = nums[nums.Length - 1];
-         for (int i = nums.Length - 1; i > 0; i++)
-         {
-            nums[i] = nums[i - 1];
-         }
-         nums[0] = temp;
+         int temp = nums[start];
+         nums[start] = nums[end];
+         nums[end] = temp;
+         start++;
+         end--;
       }
    }
 }
